30693747841745c60f62791f4eb6f668c523a0c4
james5635 <brorojame2029@gmail.com>
2025-06-22 15:21:32 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,7 +9,8 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force 
+# php artisan migrate --force 
+php artisan migrate:fresh  --seed --force      
 
 # echo "Publishing cloudinary provider..."
 # php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
